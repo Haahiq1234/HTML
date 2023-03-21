4f72df2f97c3da1c66ab1047d75721adbe1057ab
Haahiq1234 <100479413+Haahiq1234@users.noreply.github.com>
2023-03-21 15:46:20 +0500

    Update 21/3/2023 15:46:19

---
+++ Commands/Program.py
@@ -0,0 +1,44 @@
+import os;
+import time;
+
+
+def join(arr, jn):
+    name = "";
+    ln = len(arr);
+    for i in range(ln):
+        nm = str(arr[i]);
+        print(nm);
+        if i > 0:
+            nm += jn;
+        name += nm;
+    return name;
+
+def run(name):
+    os.system(f"python ../{name}.py");
+
+def add(name):
+    print(name);
+
+def push():
+    os.system("push.bat");
+
+def quitProg():
+    print("Quitting");
+    #time.sleep(2);
+    quit();
+    pass;
+funcs = {"add": add, "push": push, "quit": quitProg, "" : quitProg, "exit": quitProg, "run": run};
+
+while True:
+    keys = input("Enter Command: ").split("  ");
+    if len(keys) == 0:
+        print("quitting");
+        quit();
+    else:
+        #print(funcs);
+        keyy = keys.pop(0);
+        print(keys);
+        if len(keys) == 0:
+            funcs[keyy]();
+        else:
+            funcs[keyy](*keys);
---
+++ Commands/setup.bat
@@ -0,0 +1,10 @@
+@echo off
+
+pushd
+pip install pygame
+pip install socket
+pip install _thread
+pip install tk
+pip install mysql-connector-python
+popd
+pause
